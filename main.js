3770bdc1c71e7515bb430d7f12e3d44af835f0c4
Kent Christopher Johansen <kentchris6@gmail.com>
2022-05-01 02:09:35 +0800

    Fix some bugs

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const bodyParser = require('body-parser');
 require('dotenv').config();
 
 app.use(express.json());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 app.use(cors({
     origin: process.env.ORIGIN
-}))
+}));
+
 
 require('./LOGIN/login')(app);
 require('./PUBLIC/public')(app);
